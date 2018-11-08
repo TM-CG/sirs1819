a9c2e42b596c9b02bf862eafde369aab0888f2f0
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2018-11-08 15:09:26 +0000

    Fully integrates modules in project

---
+++ kerby/kerby-lib/src/main/java/pt/ulisboa/tecnico/sirs/kerby/CipherClerk.java
@@ -0,0 +1,66 @@
+package pt.ulisboa.tecnico.sirs.kerby;
+
+import java.util.Arrays;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Node;
+
+/**
+ * Class that handles ciphered views in different formats. Ciphered views are used
+ * to transport ciphered Kerberos tickets, authenticators, etc.
+ * 
+ * @author Miguel Pardal
+ *
+ */
+public class CipherClerk {
+
+	// creation --------------------------------------------------------------
+
+	/** Create CipheredView from arguments. */
+	public CipheredView cipherBuild(byte[] data) {
+		CipheredView view = new CipheredView();
+		view.setData(data);
+		return view;
+	}
+
+	/** Create a textual representation of the CipheredView. */
+	public String cipherToString(CipheredView view) {
+		if (view == null)
+			return "null";
+
+		StringBuilder builder = new StringBuilder();
+		builder.append("CipheredView [data=");
+		builder.append(Arrays.toString(view.getData()));
+		builder.append("]");
+		return builder.toString();
+	}
+
+	// serialization ---------------------------------------------------------
+
+	/**
+	 * Marshal ciphered view to XML document.
+	 */
+	public Node cipherToXMLNode(CipheredView view, String cipherTagName) throws JAXBException {
+		return XMLHelper.viewToXML(CipheredView.class, view, new QName(cipherTagName));
+	}
+
+	/** Marshal ciphered view to XML bytes. */
+	public byte[] cipherToXMLBytes(CipheredView view, String cipherTagName) throws JAXBException {
+		return XMLHelper.viewToXMLBytes(CipheredView.class, view, new QName(cipherTagName));
+	}
+
+	/**
+	 * Unmarshal ciphered view from XML document.
+	 */
+	public CipheredView cipherFromXMLNode(Node xml) throws JAXBException {
+		return XMLHelper.xmlNodeToView(CipheredView.class, xml);
+	}
+
+	/** Unmarshal byte array to a ciphered view object. */
+	public CipheredView cipherFromXMLBytes(byte[] bytes) throws JAXBException {
+		return XMLHelper.xmlBytesToView(CipheredView.class, bytes);
+	}
+
+}

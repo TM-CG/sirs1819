b667103dfe082026c063a8aa9ff8f19db88ba719
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2018-11-13 16:12:12 +0000

    Adds patient delete

--- MDRecords/src/main/java/pt/ulisboa/tecnico/sirs/medicalrecords/personal/Patient.java
+++ MDRecords/src/main/java/pt/ulisboa/tecnico/sirs/medicalrecords/personal/Patient.java
@@ -10,16 +10,42 @@ public class Patient extends Patient_Base {
 
     public Patient(String name, DateTime birthday, long identification) throws InvalidPersonException{
         super.checkArguments(name, birthday, identification);
+        PatientAlreadyExists(identification);
+
         setName(name);
         setBirthday(birthday);
         setIdentification(identification);
+
         FenixFramework.getDomainRoot().getSNS().addPatient(this);
     }
 
     public Patient(String name, long identification) throws InvalidPersonException{
         super.checkArguments(name, identification);
+        PatientAlreadyExists(identification);
+
         setName(name);
         setIdentification(identification);
+
         FenixFramework.getDomainRoot().getSNS().addPatient(this);
     }
+        
+    /**
+     * Checks if a Patient already exists.
+     * @param identification of the Patient
+     * @throws InvalidPersonException if the such Patient exists with same id on the system.
+     */
+    private void PatientAlreadyExists(long identification) throws InvalidPersonException {
+        for (Patient Patient: FenixFramework.getDomainRoot().getSNS().getPatientSet()) {
+            if (Patient.getIdentification() == this.getIdentification())
+                return;
+        }
+        throw new InvalidPersonException("Patient: This patient already exists!");
+    }
+
+    /** Deletes Patient from the SNS */
+    public void delete() {
+        setSNS(null);
+		deleteDomainObject();
+	}
 }
+

27a5b385e9e25a8885efb6b79c6fa7176c3c12d6
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2018-11-13 15:33:26 +0000

    Changes dml structure to have a SNS

--- MDRecords/src/main/java/pt/ulisboa/tecnico/sirs/medicalrecords/personal/Patient.java
+++ MDRecords/src/main/java/pt/ulisboa/tecnico/sirs/medicalrecords/personal/Patient.java
@@ -6,11 +6,19 @@ import pt.ist.fenixframework.FenixFramework;
 import pt.ulisboa.tecnico.sirs.medicalrecords.personal.exception.InvalidPersonException;
 
 public class Patient extends Patient_Base {
+    
 
     public Patient(String name, DateTime birthday, long identification) throws InvalidPersonException{
         super.checkArguments(name, birthday, identification);
         setName(name);
         setBirthday(birthday);
+        setIdentification(identification);
+        FenixFramework.getDomainRoot().addPatient(this);
+    }
+
+    public Patient(String name, long identification) throws InvalidPersonException{
+        super.checkArguments(name, identification);
+        setName(name);
         setIdentification(identification);
         FenixFramework.getDomainRoot().addPatient(this);
     }

46736f2f7169f7a8db1c970bc2bbca8f80c55118
Vitor Nunes <vitor.sobrinho.nunes@gmail.com>
2018-11-16 17:39:22 +0000

    Adds MDRecordsPortImpl and EMan

---
+++ mdrecords/mdrecords-ws/src/main/java/pt/ulisboa/tecnico/sirs/mdrecords/MDRecordsPortImpl.java
@@ -0,0 +1,23 @@
+package pt.ulisboa.tecnico.sirs.mdrecords;
+
+import javax.jws.WebService;
+
+/**
+ * Medical records implementation class.
+ * 
+ */
+
+ public class MDRecordsPortImpl implements MDRecordsPortType {
+
+    // end point manager
+	private MDRecordsEndpointManager endpointManager;
+
+    public MDRecordsPortImpl(MDRecordsEndpointManager endpointManager) {
+		this.endpointManager = endpointManager;
+	}
+
+    public String requestRecord(Long patientId, Long personalId) throws BadRecordRequest_Exception {
+        return null;
+    }
+
+ }

a0d0aabe0106d8caad01b1266b8fcb89dd8d07fc
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2018-12-09 16:46:25 +0000

    Implements Report as a subtype of Record

--- mdrecords/mdrecords-lib/src/main/java/pt/ulisboa/tecnico/sirs/mdrecords/personal/Report.java
+++ mdrecords/mdrecords-lib/src/main/java/pt/ulisboa/tecnico/sirs/mdrecords/personal/Report.java
@@ -1,9 +1,24 @@
 package pt.ulisboa.tecnico.sirs.mdrecords.personal;
 
+import org.joda.time.DateTime;
+import pt.ist.fenixframework.FenixFramework;
+
 public class Report extends Report_Base {
     
     public Report() {
         super();
+    }
+
+    public Report(long personalId, long patientId, DateTime timeStamp, String speciality, String description) throws InvalidRecordException {
+        checkArguments(personalId, patientId, timeStamp, speciality, description);
+
+        setPersonalId(personalId);
+        setPatientId(patientId);
+        setTimeStamp(timeStamp);
+        setSpeciality(speciality);
+        setDescription(description);
+
+        FenixFramework.getDomainRoot().getSns().addRecord(this);
     }
     
 }
